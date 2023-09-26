435080c7ba8152048750ea4e478e6d1cfef279fc
Sarah Pickrahn <spickrahn@gmail.com>
2023-09-25 22:51:36 -0700

    get responsive layout working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,43 @@
 import Image from "next/image";
 import headshot from "./headshot.jpg";
+import githubLogo from "./github-mark.png";
+import linkedinLogo from "./linkedin-logo.png";
+import { SocialLink } from "@/components/SocialLink";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
-      <div className="flex flex-wrap">
-        <div className="rounded-md">
-          <Image
-            src={headshot}
-            alt="Sarah headshot"
-            priority
-            width="300"
-            height="300"
-          />
-        </div>
-        <div className="mt-4 sm:mt-0 px-5">
-          <h1>Sarah Pickrahn</h1>
-          <p>copy: tbd</p>
-          <div className="flex flex-col text-slate-900 text-sm">
-            <a href="https://github.com/pickles27">Github</a>
-            <a href="https://www.linkedin.com/in/sarah-pickrahn/">LinkedIn</a>
-          </div>
-        </div>
+    <main className="min-h-screen sm:grid sm:grid-cols-2 sm:items-center sm:gap-3">
+      <figure className="w-full sm:rounded sm:w-80 overflow-hidden ml-auto">
+        <Image
+          src={headshot}
+          alt="Sarah headshot"
+          priority
+          placeholder="blur"
+        />
+      </figure>
+      <div className="flex flex-col gap-3 p-5 sm:w-80 text-sm">
+        <h1 className="font-serif text-xl">Sarah Pickrahn</h1>
+        <p className="text-slate-800">
+          Hi, I’m Sarah! I’m a front-end engineer living in Snohomish, WA with
+          my boyfriend and 2 cats, Zombie and Penelope. When I’m not working, I
+          love rock climbing, lifting weights, and painting.
+        </p>
+        <SocialLink
+          alt="github logo"
+          height={16}
+          linkHref="https://github.com/pickles27"
+          linkLabel="github.com/pickles27"
+          logo={githubLogo}
+          width={16}
+        />
+        <SocialLink
+          alt="linkedin logo"
+          height={16}
+          linkHref="https://www.linkedin.com/in/sarah-pickrahn"
+          linkLabel="linkedin.com/in/sarah-pickrahn"
+          logo={linkedinLogo}
+          width={19}
+        />
       </div>
     </main>
   );
